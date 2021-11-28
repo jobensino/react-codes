ff010d23c3e1e2239f29e6d5c51fc5b77b5510aa
jobensino <jobensino@gmail.com>
2021-11-28 20:06:18 -0300

    Initial files

---
+++ clean-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <p>Aplicativo Vazio</p>
+    </div>
+  );
+}
+
+export default App;
